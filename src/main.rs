2506ebfe5d3acb24baa519aa7a1413014f96465a
d2verb <licprog73@gmail.com>
2020-06-06 01:30:15 +0900

    Make prompt()

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::io::{self, Write};
 
+fn prompt(prefix: &str) -> std::io::Result<String> {
+    print!("{} ", prefix);
+    io::stdout().flush().unwrap();
+
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+
+    Ok(line.trim().to_string())
+}
+
 fn main() {
     loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-
-        let mut line = String::new();
-        io::stdin()
-            .read_line(&mut line)
-            .expect("Failed to read line");
-
-        println!("Your input is {}", line.trim());
+        let line = match prompt("$") {
+            Ok(line) => line,
+            Err(err) => {
+                println!("{}", err);
+                break;
+            }
+        };
+        println!("Your input is {}", line);
     }
 }
