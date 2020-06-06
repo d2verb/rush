522e71c0bd4bec69905866a009864241a4d3708c
d2verb <licprog73@gmail.com>
2020-06-06 18:43:46 +0900

    A lot of works

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,67 @@
-extern crate nix;
-
 use nix::sys::wait::*;
 use nix::unistd::*;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 use std::env;
 use std::ffi::CString;
-use std::io::{self, Write};
-
-fn prompt(prefix: &str) -> String {
-    print!("{} ", prefix);
-    io::stdout().flush().unwrap();
-
-    let mut line = String::new();
-    io::stdin()
-        .read_line(&mut line)
-        .expect("failed to read line");
-
-    line.trim().to_string()
-}
+use std::path::Path;
 
 #[derive(Debug, PartialEq, Eq)]
 enum CommandKind {
     Exit,
+    Cd,
+    Pwd,
     External,
+    None,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct Command {
     kind: CommandKind,
-    args: Vec<String>,
+    args: Option<Vec<String>>,
 }
 
 impl Command {
-    fn new(kind: CommandKind, args: Vec<String>) -> Self {
+    fn new(kind: CommandKind, args: Option<Vec<String>>) -> Self {
         Command { kind, args }
     }
 }
 
-fn get_command(line: String) -> Command {
+fn get_cmd(line: String) -> Command {
     let args: Vec<String> = line.split_whitespace().map(|s| s.to_string()).collect();
+
+    if args.len() < 1 {
+        return Command::new(CommandKind::None, None);
+    }
+
     let kind: CommandKind = match args[0].as_str() {
         "exit" => CommandKind::Exit,
+        "cd" => CommandKind::Cd,
+        "pwd" => CommandKind::Pwd,
         _ => CommandKind::External,
     };
-    Command::new(kind, args)
+    Command::new(kind, Some(args))
+}
+
+fn find_cmd_path(cmd_name: &str) -> String {
+    match env::var_os("PATH") {
+        Some(paths) => {
+            for path in env::split_paths(&paths) {
+                let cmd_path = Path::new(&path).join(cmd_name);
+                if cmd_path.exists() {
+                    return cmd_path.to_str().unwrap().to_string();
+                }
+            }
+            cmd_name.to_string()
+        }
+        None => cmd_name.to_string(),
+    }
 }
 
 fn execve_wrapper(args: Vec<String>) {
+    let path = find_cmd_path(&args[0]);
+    let cpath = CString::new(path).unwrap();
+
     let mut cargs = Vec::<CString>::new();
     for arg in args {
         cargs.push(CString::new(arg).unwrap());
@@ -55,7 +71,7 @@ fn execve_wrapper(args: Vec<String>) {
         .map(|(k, v)| CString::new(format!("{}={}", k, v)).unwrap())
         .collect();
 
-    execve(&cargs[0], &cargs[1..], &envs).expect(&format!("failed to execute {:?}", &cargs[0]));
+    execve(&cpath, &cargs[0..], &envs).expect(&format!("failed to execute {:?}", &cargs[0]));
 }
 
 fn exec_cmd(cmd: Command) {
@@ -63,19 +79,44 @@ fn exec_cmd(cmd: Command) {
         CommandKind::Exit => {
             std::process::exit(0);
         }
+        CommandKind::Cd => {
+            let args = cmd.args.unwrap();
+            match env::set_current_dir(&args[1]) {
+                Ok(_) => {}
+                Err(err) => println!("failed to change directory to '{:?}': {:?}", &args[1], err),
+            }
+        }
+        CommandKind::Pwd => {
+            let path = env::current_dir().unwrap();
+            println!("{}", path.display());
+        }
         CommandKind::External => match fork().expect("fork failed") {
             ForkResult::Parent { child } => {
                 let _ = waitpid(child, None);
             }
-            ForkResult::Child => execve_wrapper(cmd.args),
+            ForkResult::Child => execve_wrapper(cmd.args.unwrap()),
         },
+        CommandKind::None => {}
     }
 }
 
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+    let mut rl = Editor::<()>::new();
+
     loop {
-        let line = prompt("$");
-        let cmd = get_command(line);
-        exec_cmd(cmd);
+        let line = rl.readline("$ ");
+        match line {
+            Ok(line) => {
+                let cmd = get_cmd(line);
+                exec_cmd(cmd);
+            }
+            Err(ReadlineError::Interrupted) => break,
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("error: {:?}", err);
+                break;
+            }
+        }
     }
 }
